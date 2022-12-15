d2aa30fe317fac3980b06181743397296b479e7b
ramasdetqa <115881094+ramasdetqa@users.noreply.github.com>
2022-12-14 20:03:12 -0600

    package Rama created
    Created to update rama code

---
+++ src/main/java/com/sira/learning/javalearningdec22/rama/App.java
@@ -0,0 +1,14 @@
+package com.sira.learning.javalearningdec22.rama;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println( "Hi.. This is Rama" );   
+    	System.out.println( "Hello World!" );
+    }
+}
